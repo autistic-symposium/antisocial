8344099a2a99e7926416e89987a1410602ca4bee
Mari Wahl <mari.wahl9@gmail.com>
2014-09-05 11:19:17 -0400

    list of blogs done

--- app/main/views.py
+++ app/main/views.py
@@ -29,8 +29,12 @@ def index():
 # profile page route
 @main.route('/user/<username>')
 def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+            abort(404)
+    # display blogs for the user
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 
 
