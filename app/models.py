758f505e67807583802c6e697e439e3d38ae8d6e
Mari Wahl <mari.wahl9@gmail.com>
2014-09-09 14:19:46 -0400

    comments in posts

--- app/models.py
+++ app/models.py
@@ -134,6 +134,9 @@ class User(UserMixin, db.Model):
                                 lazy='dynamic',
                                 cascade='all, delete-orphan')
 
+    # Comments in posts
+    comments = db.relationship('Comment', backref='author', lazy='dynamic')
+
 
     # show own posts together with follower points
     @staticmethod
@@ -337,6 +340,7 @@ class Post(db.Model):
     body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    comments = db.relationship('Comment', backref='post', lazy='dynamic')
 
 
     # create fake posts
@@ -417,3 +421,26 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+"""
+    COMMENTS class
+"""
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    disabled = db.Column(db.Boolean)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'code', 'em', 'i',
+                        'strong']
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html'),
+            tags=allowed_tags, strip=True))
+
+db.event.listen(Comment.body, 'set', Comment.on_changed_body)
