3c87c0f134f7c38771a91688f50578d56ce704b3
Mari Wahl <mari.wahl9@gmail.com>
2014-09-09 13:43:07 -0400

    followers complete

--- app/models.py
+++ app/models.py
@@ -135,6 +135,16 @@ class User(UserMixin, db.Model):
                                 cascade='all, delete-orphan')
 
 
+    # show own posts together with follower points
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
+
+
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -150,6 +160,8 @@ class User(UserMixin, db.Model):
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = hashlib.md5(
                 self.email.encode('utf-8')).hexdigest()
+        self.followed.append(Follow(followed=self))
+
 
 
     # password hashing and unhashing
