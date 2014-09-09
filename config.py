6a3e95817b6b87f816f31f1da56d950e4ad6f67e
Mari Wahl <mari.wahl9@gmail.com>
2014-09-09 17:58:49 -0400

    heroku

--- config.py
+++ config.py
@@ -81,9 +81,9 @@ class ProductionConfig(Config):
                 secure = ()
         mail_handler = SMTPHandler(
             mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
-            fromaddr=cls.FLASKY_MAIL_SENDER,
-            toaddrs=[cls.FLASKY_ADMIN],
-            subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Application Error',
+            fromaddr=cls.ANTISOCIAL_MAIL_SENDER,
+            toaddrs=[cls.ANTISOCIAL_ADMIN],
+            subject=cls.ANTISOCIAL_MAIL_SUBJECT_PREFIX + ' Application Error',
             credentials=credentials,
             secure=secure)
         mail_handler.setLevel(logging.ERROR)
